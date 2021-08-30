4739372018040d96372c3dbe0bc1c3b7d3f3c4b0
Joe Schr <joe.vandelay@gmail.com>
2021-08-30 13:18:43 +0200

    fix: typo

--- 9_output.tf
+++ 9_output.tf
@@ -16,7 +16,7 @@ output "instance_user" {
   value = var.instance_user
 }
 
-output "insance_user_password" {
+output "instance_user_password" {
   description = "instance(s) user password" 
   value = random_password.instance_user_password.result
 }
