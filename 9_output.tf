c9846ea8ff6692667ef9a2e865fafea698f9ccaf
Joe Schr <joe.vandelay@gmail.com>
2021-10-04 21:05:02 +0200

    wip: add kubernet cluster via k3sup

--- 9_output.tf
+++ 9_output.tf
@@ -1,6 +1,10 @@
-output "virtual_machine_default_ips" {
-  description = "The default IP address of each instance deployed, indexed by name."
+output "k3s_master" {
 
+  description = "The default IP address of the k3s controller."
+  value = tomap( { "${vultr_instance.k3s_master_instance.hostname}" = vultr_instance.k3s_master_instance.main_ip })
+}
+output "k3s_nodes" {
+  description = "The default IP address of each node deployed, indexed by name."
   value = zipmap(
     flatten(tolist(
       vultr_instance.instance.*.hostname,
@@ -12,12 +16,12 @@ output "virtual_machine_default_ips" {
 }
 
 output "instance_user" {
-  description = "instance(s) user login" 
+  description = "instance(s) user login"
   value = var.instance_user
 }
 
 output "instance_user_password" {
-  description = "instance(s) user password" 
+  description = "instance(s) user password"
   value = random_password.instance_user_password.result
   sensitive = true
 }
