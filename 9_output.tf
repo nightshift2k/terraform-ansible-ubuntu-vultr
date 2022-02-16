57a809cd4af6f51334f290a8026bbee2eb42fb4b
Denys Sokolov <hantden@gmail.com>
2022-02-16 04:06:45 +0200

    update provisioner

--- 9_output.tf
+++ 9_output.tf
@@ -2,21 +2,21 @@ output "virtual_machine_default_ips" {
   description = "The default IP address of each instance deployed, indexed by name."
 
   value = zipmap(
-    flatten(list(
+    flatten(tolist(
       vultr_instance.instance.*.hostname,
     )),
-    flatten(list(
+    flatten(tolist(
       vultr_instance.instance.*.main_ip,
     )),
   )
 }
 
 output "instance_user" {
-  description = "instance(s) user login" 
-  value = var.instance_user
+  description = "instance(s) user login"
+  value       = var.instance_user
 }
 
 output "insance_user_password" {
-  description = "instance(s) user password" 
-  value = random_password.instance_user_password.result
+  description = "instance(s) user password"
+  value       = nonsensitive(random_password.instance_user_password.result)
 }
